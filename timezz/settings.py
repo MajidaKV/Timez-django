b7663fdc07b96d0bdbc673717d4799c084b4da6d
Majida <majidakv@gmail.com>
2022-08-13 09:53:58 +0530

    static

--- timezz/settings.py
+++ timezz/settings.py
@@ -132,9 +132,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR /'static'
+# STATIC_ROOT = BASE_DIR /'static'
 STATICFILES_DIRS=[
-    BASE_DIR /'timezz/static'
+    BASE_DIR /'static'
 ]
 
 # media files configaration
@@ -167,6 +167,7 @@ KEY = 'rzp_test_NNYgytwoxPgefB'
 SECRET ='93YDoFVC5WOPBN7CXkBFynBr'
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
 
 AWS_QUERYSTRING_AUTH = False
 AWS_S3_FILE_OVERWRITE = True
